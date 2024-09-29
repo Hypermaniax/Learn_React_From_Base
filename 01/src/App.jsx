8aac7451d02e699aaccf0c2583934a87d0255356
niko <nikoagustio22@gmail.com>
2024-09-30 00:31:36 +0700

    have to put key in CoreConcept for rect , because have a uniquely identifies

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -18,7 +18,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((concept)=>(<CoreConcept {...concept}/>))}
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
           </ul>
         </section>
         <section id="examples">
